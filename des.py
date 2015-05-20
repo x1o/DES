3c661e543b35e9654912ede3326f54da275c405a
Dmitry Zotikov <xio@ungrund.org>
2015-05-20 16:02:05 +0300

    initial

---
+++ des.py
@@ -0,0 +1,214 @@
+#!/usr/bin/env python3
+
+import sys
+
+NULL = '@'
+
+# Initial Permutation
+IP = [58,50,42,34,26,18,10,2,60,52,44,36,28,20,12,4,
+      62,54,46,38,30,22,14,6,64,56,48,40,32,24,16,8,
+      57,49,41,33,25,17,9,1,59,51,43,35,27,19,11,3,
+      61,53,45,37,29,21,13,5,63,55,47,39,31,23,15,7]
+
+# Permuted Choice - 1
+PC_1 = [57,49,41,33,25,17,9,1,58,50,42,34,26,18,10,2,
+        59,51,43,35,27,19,11,3,60,52,44,36,63,55,47,39,
+        31,23,15,7,62,54,46,38,30,22,14,6,61,53,45,37,
+        29,21,13,5,28,20,12,4]
+
+# Permuted Choice - 2
+PC_2 = [14,17,11,24,1,5,3,28,15,6,21,10,23,19,12,4,
+        26,8,16,7,27,20,13,2,41,52,31,37,47,55,30,40,
+        51,45,33,48,44,49,39,56,34,53,46,42,50,36,29,32]
+
+# Expansion permutation
+E = [32,1,2,3,4,5,4,5,6,7,8,9,8,9,10,11,
+     12,13,12,13,14,15,16,17,16,17,18,19,20,21,20,21,
+     22,23,24,25,24,25,26,27,28,29,28,29,30,31,32,1]
+
+# S-boxes
+Si = [
+        [14,4,13,1,2,15,11,8,3,10,6,12,5,9,0,7,
+         0,15,7,4,14,2,13,1,10,6,12,11,9,5,3,8,
+         4,1,14,8,13,6,2,11,15,12,9,7,3,10,5,0,
+         15,12,8,2,4,9,1,7,5,11,3,14,10,0,6,13],
+        [15,1,8,14,6,11,3,4,9,7,2,13,12,0,5,10,
+         3,13,4,7,15,2,8,14,12,0,1,10,6,9,11,5,
+         0,14,7,11,10,4,13,1,5,8,12,6,9,3,2,15,
+         13,8,10,1,3,15,4,2,11,6,7,12,0,5,14,9],
+        [10,0,9,14,6,3,15,5,1,13,12,7,11,4,2,8,
+         13,7,0,9,3,4,6,10,2,8,5,14,12,11,15,1,
+         13,6,4,9,8,15,3,0,11,1,2,12,5,10,14,7,
+         1,10,13,0,6,9,8,7,4,15,14,3,11,5,2,12],
+        [7,13,14,3,0,6,9,10,1,2,8,5,11,12,4,15,
+         13,8,11,5,6,15,0,3,4,7,2,12,1,10,14,9,
+         10,6,9,0,12,11,7,13,15,1,3,14,5,2,8,4,
+         3,15,0,6,10,1,13,8,9,4,5,11,12,7,2,14],
+        [2,12,4,1,7,10,11,6,8,5,3,15,13,0,14,9,
+         14,11,2,12,4,7,13,1,5,0,15,10,3,9,8,6,
+         4,2,1,11,10,13,7,8,15,9,12,5,6,3,0,14,
+         11,8,12,7,1,14,2,13,6,15,0,9,10,4,5,3],
+        [12,1,10,15,9,2,6,8,0,13,3,4,14,7,5,11,
+         10,15,4,2,7,12,9,5,6,1,13,14,0,11,3,8,
+         9,14,15,5,2,8,12,3,7,0,4,10,1,13,11,6,
+         4,3,2,12,9,5,15,10,11,14,1,7,6,0,8,13],
+        [4,11,2,14,15,0,8,13,3,12,9,7,5,10,6,1,
+         13,0,11,7,4,9,1,10,14,3,5,12,2,15,8,6,
+         1,4,11,13,12,3,7,14,10,15,6,8,0,5,9,2,
+         6,11,13,8,1,4,10,7,9,5,0,15,14,2,3,12],
+        [13,2,8,4,6,15,11,1,10,9,3,14,5,0,12,7,
+         1,15,13,8,10,3,7,4,12,5,6,11,0,14,9,2,
+         7,11,4,1,9,12,14,2,0,6,10,13,15,3,5,8,
+         2,1,14,7,4,10,8,13,15,12,9,0,3,5,6,11]
+]
+
+# Permutation (last step in Feistel)
+P = [16,7,20,21,29,12,28,17,1,15,23,26,5,18,31,10,
+     2,8,24,14,32,27,3,9,19,13,30,6,22,11,4,25]
+
+# Final Permutation
+FP = [40,8,48,16,56,24,64,32,39,7,47,15,55,23,63,31,
+      38,6,46,14,54,22,62,30,37,5,45,13,53,21,61,29,
+      36,4,44,12,52,20,60,28,35,3,43,11,51,19,59,27,
+      34,2,42,10,50,18,58,26,33,1,41,9,49,17,57,25]
+
+
+def chunks(l, n):
+    for i in range(0, len(l), n):
+        yield l[i:i+n]
+
+
+def glue(iterable):
+    return ''.join(iterable)
+
+
+def char_to_bin(char):
+    return bin(ord(char))[2:].rjust(8, '0') 
+
+
+def str_to_bin(s):
+    return glue([char_to_bin(char) for char in s])
+
+
+def int_to_bin(n, pad_len=8):
+    return bin(n)[2:].rjust(pad_len, '0')
+
+
+def bin_to_str(bs):
+    return glue([chr(int(letter, 2)) for letter in list(chunks(bs, 8))])
+
+
+def permute_by_table(s, table, resize=False):
+    if resize:
+        s_new = [None for _ in range(len(table))]
+    else:
+        s_new = list(s)
+    for i in range(len(table)):
+        s_new[i] = s[table[i]-1]
+    return s_new
+
+
+def xor(s1, s2, pad_len=None):
+    if pad_len is None:
+        pad_len = max(len(s1), len(s2))
+    return int_to_bin(int(s1, 2) ^ int(s2, 2), pad_len)
+
+
+def byteshift(s, n):
+    return s[n:] + s[:n]
+
+
+def key_schedule(key, round_n):
+    """ key: 64 bit long binary key as a string
+        round_n: round number
+    """
+
+    key = permute_by_table(key, PC_1)
+    del key[::8]
+    key = glue(key)
+
+    key = [key[:len(key)//2], key[len(key)//2:]]
+
+    if round_n in {1, 2, 9, 16}:
+        shift_step = 1
+    else:
+        shift_step = 2
+
+    key = glue([byteshift(half, shift_step) for half in key])
+    key = glue(permute_by_table(key, PC_2, True))
+
+    return key
+
+
+def Feistel(hblock, key, round_n):
+    # --- 1. Expansion permutation ---
+
+    hblock = glue(permute_by_table(hblock, E, True))
+
+    # --- 2. Key mixing ---
+
+    key = key_schedule(key, round_n)
+    hblock = xor(hblock, key, len(hblock))
+
+    # --- 3. Substitution ---
+
+    hblock = list(chunks(hblock, 6))
+
+    for i in range(len(hblock)):
+        chunk = hblock[i]
+        row_n = int(chunk[0] + chunk[-1], 2)
+        col_n = int(chunk[1:-1], 2)
+        offset = row_n * 16
+        output = bin(Si[i][offset+col_n])[2:].rjust(4, '0')
+        hblock[i] = output
+
+    # --- 4. Permutation ----
+
+    hblock = glue(permute_by_table(glue(hblock), P))
+
+    return hblock
+
+
+try:
+    S = sys.argv[1]
+except IndexError:
+    S = 'Cryptography'
+
+# print('Input string: %s' % S)
+
+try:
+    key = sys.argv[2]
+    if len(key) < 8:
+        raise Exception('Key should be 8-bytes long.')
+except IndexError:
+    key = 'infinity'
+
+key = str_to_bin(key)
+
+
+# --- Pre-processing ---
+
+blocks = list(chunks(S, 8))
+blocks[-1] = blocks[-1].ljust(8, NULL)
+blocks = [str_to_bin(block) for block in blocks]
+
+# --- Initial Permutation and Splitting ---
+
+blocks = [glue(permute_by_table(block, IP)) for block in blocks]
+blocks = [[block[:32], block[32:]] for block in blocks]
+
+# --- Main rounds ---
+
+ciphertext = ''
+
+for block in blocks:
+    L, R = block
+    for round_n in range(16):
+        F = Feistel(R, key, round_n)
+        L = R
+        R = xor(L, F)
+    ciphertext += glue(permute_by_table(L + R, FP))
+
+ascii_ciphertext = bin_to_str(ciphertext)
+
+print(ascii_ciphertext)
